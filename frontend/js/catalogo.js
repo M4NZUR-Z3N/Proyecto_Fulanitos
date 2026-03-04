7045f647a84b0a3d8cb85f13f6536cc3c428b956
M4NZUR <mbenavidessa@ucenfotec.ac.cr>
2026-03-04 01:18:46 -0600

    Corrección bootstrap carrito

--- frontend/js/catalogo.js
+++ frontend/js/catalogo.js
@@ -7,6 +7,8 @@ const botonMas = document.getElementById('boton-mas');
 const botonMenos = document.getElementById('boton-menos');
 const spanCantidad = document.getElementById('cantidad');
 
+const discos = document.getElementById('discos');
+
 let cantidad = 1; //Cantidad de producto seleccionada que despues se añadirá al carrito cuando sea funcional
 
 //Funciones
@@ -36,6 +38,23 @@ function añadirAlCarrito() {
     console.log('Añadido al carrito: ' + cantidad + ' unidades');
 }
 
+//Funcion para cargar productos iguales como version de prueba, luego se sustituye por lo que recibamos del backend
+function mostrarProductos() {
+    for (let i = 0; i < 20; i++) {
+        const producto = document.createElement('div');
+        producto.classList.add('btn', 'mt-5', 'col-lg-2', 'col-md-3', 'col-6');
+        producto.innerHTML = `
+        <img class="img-fluid shadow-lg rounded" src="/assets/images/portadas/vinilo-base.webp"
+            alt="Vinilo Base">
+        <div class="d-flex justify-content-between">
+            <div>Vinilo Base</div>
+            <div>$10</div>
+        </div>
+    `;
+        discos.appendChild(producto);
+    }
+}
+
 //EventListeners
 
 //Abre los detalles del producto
@@ -43,9 +62,16 @@ disco.addEventListener('click', function () {
     abrirDetalles();
 });
 
-//Cierra los detalles del producto
+//Cierra los detalles del producto al hacer click en el fondo oscuro
 bgBorroso.addEventListener('click', function () {
     cerrarDetalles();
+});
+
+//Cierra los detalles si se hace click fuera del cuadro blanco (en el contenedor wrapper)
+detalles.addEventListener('click', function (e) {
+    if (e.target === detalles) {
+        cerrarDetalles();
+    }
 });
 
 //Aumenta la cantidad de producto seleccionado
@@ -63,3 +89,5 @@ botonAñadir.addEventListener('click', function () {
     añadirAlCarrito();
     cerrarDetalles();
 });
+
+mostrarProductos();
